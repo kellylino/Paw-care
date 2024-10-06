4da7e21ea08ecf315a710b19fb04236c93a698a6
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-10-06 12:45:27 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['Owner', 'Giver'], required: null },
+    role: { type: String, enum: ['Owner', 'Giver'], default: null },
   },
   { timestamps: true }
 );
